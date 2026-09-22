b15a19c394cc09b0c9f42590b29732b115420da7
alex <alex@pdp7.net>
2026-09-22 18:15:09 +0200

    Remove bad unwrap

--- src/bin/check_url.rs
+++ src/bin/check_url.rs
@@ -4,25 +4,35 @@ pub fn main() {
     let args = std::env::args().collect::<Vec<_>>();
     assert_eq!(args.len(), 2, "expected a single argument");
     let url = &args[1];
-    let status = ureq::Agent::config_builder()
+    let (status, description) = match ureq::Agent::config_builder()
         .http_status_as_error(false)
         .build()
         .new_agent()
         .get(url)
         .call()
-        .expect("url call not to fail")
-        .status();
+    {
+        Ok(response) => (
+            {
+                if response.status().is_success() {
+                    monitoring::SubCheckStatus::OK
+                } else {
+                    monitoring::SubCheckStatus::CRITICAL
+                }
+            },
+            format!("request to {url} returned {}", response.status()),
+        ),
+        Err(err) => (
+            monitoring::SubCheckStatus::CRITICAL,
+            format!("error {err} requesting {url}"),
+        ),
+    };
     std::io::stderr()
         .write(
             serde_json::to_string(&monitoring::CheckResult {
                 subchecks: vec![monitoring::SubCheckResult {
                     id: "request".into(),
-                    status: if status.is_success() {
-                        monitoring::SubCheckStatus::OK
-                    } else {
-                        monitoring::SubCheckStatus::CRITICAL
-                    },
-                    description: format!("request to {url} returned {status}"),
+                    status,
+                    description,
                 }],
             })
             .unwrap()
